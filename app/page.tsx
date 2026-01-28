d9b58c8ffa24007f347e69d6ad1f4e48666503a4
edgifytradingcompany-hash <edgifytradingcompany@gmail.com>
2026-01-27 17:16:36 -0700

    Add Home component with welcome message

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Edgify</h1>
+      <p>Welcome to the Edgify marketplace.</p>
+    </main>
+  );
+}
 
